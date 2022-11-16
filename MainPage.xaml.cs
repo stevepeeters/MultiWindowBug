8c243e47a551be814da4c8cc9346d8069621177f
user <user@github.com>
2022-11-16 18:23:13 +0100

    content

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnOpenWindowClicked(object sender, EventArgs e)
+	{
+		ContentPage dummyPage = new()
+		{
+			Content = new Label() { Text = "Dummy Label", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center }
+		};
+		Window secondWindow = new(dummyPage);
+		Application.Current.OpenWindow(secondWindow);
+	}
+
+	private async void OnDisplayAlertClicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("Alert", "Hello World.", "OK");
+	}
+}
+
